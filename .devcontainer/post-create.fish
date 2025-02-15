0eb5914d8a482df16d789c9bda8c03faf248d04f
= <=>
2025-02-15 03:17:13 +0000

    fix: tell the user to input their git credentials when building the devcontainer

--- .devcontainer/post-create.fish
+++ .devcontainer/post-create.fish
@@ -9,3 +9,10 @@ pip install -r src/requirements.txt
 # Make the shell look nice :)
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher install IlanCosman/tide@v6
+
+# tell the user to input their git credentials
+echo "
+Don't forget to update your git credentials! Use the following commands to update your git credentials: 
+    git config --global user.email \"your username\" 
+    git config --global user.name \"your email\"
+"
