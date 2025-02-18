75b13c788a182099e2cba6a0e2d4eb3417051bb1
Alec <50636059+alec-zaiane@users.noreply.github.com>
2025-02-18 18:46:00 +0000

    fix: fix rebase issue

--- src/socialnetwork/views.py
+++ src/socialnetwork/views.py
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-from typing import Any, Literal
-
-from django.shortcuts import render
-=======
-from typing import Optional
+from typing import Any, Literal, Optional
 
 from django.shortcuts import render, get_object_or_404
->>>>>>> origin/feat/profile-page
-=======
-from typing import Optional, Any, Literal
-
-from django.shortcuts import render, get_object_or_404
->>>>>>> 9688107 (Merge remote-tracking branch 'origin/feat/profile-page' into feat/create-posts)
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from rest_framework.decorators import api_view # type: ignore # missing stub file
 from rest_framework.response import Response # type: ignore # missing stub file
