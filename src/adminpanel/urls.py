b3563eeed6650004b2f53f26edbf1f552398816a
Alec <50636059+alec-zaiane@users.noreply.github.com>
2025-02-23 19:56:29 +0000

    refactor: update URLS to align with others

--- src/adminpanel/urls.py
+++ src/adminpanel/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path("api/join_request/<int:join_request_id>/deny", views.api_join_request_deny, name="api_join_request_deny"),
     path("api/join_request/<int:join_request_id>/undeny", views.api_join_request_undeny, name="api_join_request_undeny"),
     path("api/join_request/<int:join_request_id>/delete", views.api_join_request_delete, name="api_join_request_delete"),
-    path('hosted_images/', views.api_hosted_images, name='hosted_images_api'),
-    path('hosted_images/<int:image_id>/', views.api_delete_hosted_image, name='delete_hosted_image_api'),
+    path('api/hosted_images/', views.api_hosted_images, name='hosted_images_api'),
+    path('api/hosted_images/<int:image_id>/delete', views.api_delete_hosted_image, name='delete_hosted_image_api'),
 ]
