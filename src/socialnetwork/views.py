d5f6ac6e1f15ca74202af3e0caa5714585069b41
dottemeod <mirimaadkhan@gmail.com>
2025-02-23 18:29:29 -0700

    feat: Deletion of posts
    Authors can now delete their own posts, they are prompted twice to ensure they want to delete a post.
    This is a soft delete so that it admins can still see removed posts.

--- src/socialnetwork/views.py
+++ src/socialnetwork/views.py
@@ -1,7 +1,7 @@
 from typing import Any, Literal, Optional
 
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
@@ -43,7 +43,7 @@ def author_profile_view(request: HttpRequest, target_author_uuid: str, author: O
     """View `target_author_uuid`'s profile"""
 
     target_author = get_object_or_404(models.LocalAuthor, uuid=target_author_uuid)
-    author_posts = models.PostTextBased.objects.filter(base_author=target_author)
+    author_posts = models.PostTextBased.objects.filter(base_author=target_author, is_deleted=False)
 
     return render(request, "author_profile.html", {"author": target_author, "viewer": author, "posts": author_posts})
 
@@ -95,3 +95,17 @@ def api_author_update(request:Request, target_author_uuid:str, author:Optional[m
 def create_post_view(request: HttpRequest, author: models.LocalAuthor) -> HttpResponse:
     """Render a form for authors to create a post."""
     return render(request, "create_post.html", {"author": author})
+    
+    
+@user_control(can_be_author=True, can_be_logged_out=False, can_be_superuser=False)
+def delete_post_view(request: HttpRequest, author: models.LocalAuthor, post_uuid: str) -> HttpResponse:
+    post = get_object_or_404(models.PostTextBased, uuid=post_uuid)
+    
+    # Only the post's owner can delete
+    if post.author != author:
+        return HttpResponseForbidden("You don't have permission to delete this post.")
+    
+    # Perform the soft delete
+    # this is ssetting is_deleted to = True which is added field to author_posts above
+    post.delete()
+    return redirect("socialnetwork:author_profile", target_author_uuid=author.uuid)
