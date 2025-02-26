33f770e3bf7419ed280255af7f3d049aa1efa633
Alec <50636059+alec-zaiane@users.noreply.github.com>
2025-02-26 00:07:33 +0000

    refactor: move all API views into their own file (per django app)

--- src/socialnetwork/urls.py
+++ src/socialnetwork/urls.py
@@ -1,24 +1,52 @@
 from django.urls import path
 from django.views.generic import RedirectView
 from . import views
+from . import views_api
 
 app_name = "socialnetwork"
 urlpatterns = [
     path("", RedirectView.as_view(url="stream")),
-    path("not_logged_in", views.not_logged_in_view, name="not_logged_in"),
-    path("stream", views.stream_view, name="stream"),
+
+    path("not_logged_in",
+         views.not_logged_in_view,
+         name="not_logged_in"),
+
+    path("stream",
+         views.stream_view,
+         name="stream"),
+
     path("author/<uuid:target_author_uuid>",
-         views.author_profile_view, name="author_profile"),
+         views.author_profile_view,
+         name="author_profile"),
+
     path("author/<uuid:target_author_uuid>/modify",
-         views.local_author_modify_view, name="author_modify"),
-    path("create_post", views.create_post_view, name="author_create_post"),
-    path("edit_post/<uuid:post_uuid>", views.edit_post_view, name="edit_post"),
+         views.local_author_modify_view,
+         name="author_modify"),
+
+    path("create_post",
+         views.create_post_view,
+         name="author_create_post"),
+
+    path("edit_post/<uuid:post_uuid>",
+         views.edit_post_view,
+         name="edit_post"),
 
 
-    path("api/post/delete/<uuid:post_uuid>",
-         views.api_post_delete, name="api_post_delete"),
-    path("api/create_text_post", views.api_create_text_post,
-         name="api_create_text_post"),
-    path("api/author/update/<uuid:target_author_uuid>",
-         views.api_author_update, name="api_author_update"),
 ]
+
+urlpatterns_api = [
+    path("api/post/delete/<uuid:post_uuid>",
+         views_api.api_post_delete,
+         name="api_post_delete"),
+
+    path("api/create_text_post",
+         views_api.api_create_text_post,
+         name="api_create_text_post"),
+
+    path("api/author/update/<uuid:target_author_uuid>",
+         views_api.api_author_update,
+         name="api_author_update"),
+
+]
+
+urlpatterns.extend(urlpatterns_api)
---
+++ src/socialnetwork/views_api.py
@@ -0,0 +1,121 @@
+from __future__ import annotations
+
+from typing import Optional
+
+from django.shortcuts import get_object_or_404
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+
+from socialnetwork.utils.user_control_decorator import user_control
+from socialnetwork import serializers
+from socialnetwork import models
+
+
+@api_view(["POST"])
+@user_control(can_be_author=True, can_be_logged_out=False, can_be_superuser=False)
+def api_create_text_post(request: Request, author: models.LocalAuthor) -> Response:
+    """Create a text based post
+    Will serve a redirect if unauthorized
+    Expects JSON
+        {
+            "content":<post content>:str
+            "visibility_type":<either 'PU' for public, 'FO' for Friends Only, 'UN' for unlisted>:str
+            "post_type":<either 'PT' for plaintext, or 'MD' for markdown>:str
+        }
+    returns JSON on success (code 201) mirrorring input
+    returns JSON on failure (code 400) mirroring expected keys, with values as errors
+    """
+    data = request.data.copy()
+    data["base_author"] = author.uuid
+
+    serializer = serializers.PostTextBasedSerializer(data=data)
+
+    # save it all
+    if serializer.is_valid():
+        post = serializer.save()
+        post.send_to_required_private_inboxes()
+        return Response(serializer.data, status=201)
+    else:
+        return Response(serializer.errors, status=400)
+
+
+@api_view(["PUT", "PATCH"])
+@user_control(can_be_author=True, can_be_logged_out=False, can_be_superuser=True)
+def api_author_update(request: Request, target_author_uuid: str, author: Optional[models.LocalAuthor] = None) -> Response:
+    """Update an author's information, **author kwarg is not the target author, but the viewer author**
+    Only an admin or the author themselves can update their information"
+
+    Will serve a redirect if unauthorized
+    expects JSON
+        {
+            "following":<list of author uuids>:list[str] (optional)
+            "username":<updated username>:str (optional)
+            "first_name":<updated first name>:str (optional)
+            "last_name":<updated last name>:str (optional)
+            "email":<updated email>:str (optional)
+        }
+    Alternative JSON
+        {
+            "user" {
+                "username":<updated username>:str (optional)
+                "first_name":<updated first name>:str (optional)
+                "last_name":<updated last name>:str (optional)
+                "email":<updated email>:str (optional)
+            }
+            "following":<list of author uuids>:list[str] (optional)
+        }
+
+    will return a 404 on not found
+    returns JSON on failure (code 403)
+        {
+            "error": "You do not have permission to update this author"
+        }
+    returns JSON on failure (code 400)
+        Keys matching input fields, with values as errors
+
+    returns JSON on success (code 200)
+        JSON matching input
+    """
+    target_author = get_object_or_404(
+        models.LocalAuthor, uuid=target_author_uuid)
+    if author != target_author and not request.user.is_superuser:
+        return Response({"error": "You do not have permission to update this author"}, status=403)
+    serializer = serializers.LocalAuthorSerializer(
+        target_author, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.update(target_author, request.data.copy())
+        return Response(serializer.data, 200)
+    return Response(serializer.errors, status=400)
+
+
+@api_view(["POST"])
+@user_control(can_be_author=True, can_be_logged_out=False, can_be_superuser=False)
+def api_post_delete(request: Request, author: models.LocalAuthor, post_uuid: str) -> Response:
+    """Delete a post
+    Will serve a redirect if unauthorized
+
+    Expects no body
+    Will serve a 404 on not found
+
+    returns JSON on error (code 403)
+        {
+            "error": <error>:str
+        }
+
+    returns JSON on success (code 200)
+        {
+            "success": <success>:str
+        }
+    """
+    post = get_object_or_404(models.PostTextBased, uuid=post_uuid)
+
+    # Only the post's owner can delete
+    if post.author != author:
+        return Response({"error": "You must be the author of this post to delete it"}, 403)
+
+    # Perform the soft delete
+    # this is ssetting is_deleted to = True which is added field to author_posts above
+    post.delete()
+    return Response({"success": "Post deleted successfully"}, 200)
