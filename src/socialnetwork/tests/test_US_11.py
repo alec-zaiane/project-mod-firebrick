1c0115304938d937b8a7876473bb549e02ee937c
Alec <50636059+alec-zaiane@users.noreply.github.com>
2025-03-09 00:08:29 +0000

    test/verify post updates in DB in US 11

--- src/socialnetwork/tests/test_US_11.py
+++ src/socialnetwork/tests/test_US_11.py
@@ -1,6 +1,7 @@
 from django.test import tag
 from django.urls import reverse
 from .utils_for_tests import GeneralUserStoryApiTest
+from socialnetwork.models import PostTextBased
 
 
 @tag("US-Posting")
@@ -14,24 +15,28 @@ class TestUserStory11(GeneralUserStoryApiTest):
     def test_edit_post_content_via_api(self) -> None:
         """Test that an author can edit their post locally using the API."""
 
-        #init author and a Post
+        # init author and a Post
         self.initialize_sample_authors(1)
         self.initialize_sample_text_posts(posts_per_author=1)
 
         author = self.sample_authors[0]
 
-        #get first Post
+        # get first Post
         post = self.sample_posts[0][0]
         new_content = "This is the updated post content with the typo fixed."
 
-        #authenticate as the post's author
+        # authenticate as the post's author
         self.client.force_authenticate(user=author.user)
 
-        #call api to edit the Post
+        # call api to edit the Post
         url = reverse("socialnetwork:api_textpost_update", args=[post.uuid])
-        response = self.client.post(url, {"content": new_content}, format="json")
+        response = self.client.post(
+            url, {"content": new_content}, format="json")
 
-        #check response
+        # check response
         self.assertEqual(response.status_code, 200)
 
-
+        # make sure the content was updated
+        post.refresh_from_db()
+        assert isinstance(post, PostTextBased)
+        self.assertEqual(post.content, new_content)
