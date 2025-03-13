4d83326f638fb27fc2195714d75536688aa6452b
= <=>
2025-03-13 05:40:35 +0000

    ci: another gh actions test

--- src/core/utils/testing_utils.py
+++ src/core/utils/testing_utils.py
@@ -101,7 +101,7 @@ class UITestCase(LiveServerTestCase):
         # https://stackoverflow.com/questions/73973332/check-if-were-in-a-github-action-travis-ci-circle-ci-etc-testing-environme
         is_actions_runner = os.getenv("GITHUB_ACTIONS")
         if is_actions_runner:
-            geckodriver_path = "/snap/bin/geckodriver"
+            geckodriver_path = "/usr/local/bin/geckodriver"
 
             driver_service = webdriver.FirefoxService(
                 executable_path=geckodriver_path)
