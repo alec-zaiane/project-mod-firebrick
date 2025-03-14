b624ecfc127df765dc7e94814046ed4b88e235c6
Alec <50636059+alec-zaiane@users.noreply.github.com>
2025-03-14 16:22:34 +0000

    fix: fix double "ok" button press on test_fail_on_add_existing_username

--- src/user_management/tests/test_US_44.py
+++ src/user_management/tests/test_US_44.py
@@ -47,7 +47,6 @@ class TestUserStory44(AdminUITestCase):
         )
         self.end_test()
 
-    @skip("not implemented - this test is how it should be done")
     @tag("check-slow")
     def test_fail_on_add_existing_username(self) -> None:
         """Test that you cannot add another author with the same username"""
@@ -69,7 +68,6 @@ class TestUserStory44(AdminUITestCase):
         self.visit("/admin/user_management/joinrequest/")
         self.find_elements_by_value(str(join_request_2.uuid))[0].click()
         self.adminpanel_do_action("Approve selected join requests")
-        self.find_element_by_name("index").click()
         # ...and make sure it fails
         messages = self.find_elements_by_selector("ul.messagelist")
         self.assertIn("is already taken", messages[0].element.text)
