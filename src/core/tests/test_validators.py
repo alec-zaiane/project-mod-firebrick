69b6f6d90404e6bc53e0cd2d787e878c1a5b9301
Alec <50636059+alec-zaiane@users.noreply.github.com>
2025-03-19 17:09:54 +0000

    feat: add user agent and "accept" header to image fetch requests, fixes imgur links

---
+++ src/core/tests/test_validators.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+from django.core.exceptions import ValidationError
+
+from core.utils.validators import validate_url_returns_image
+
+
+class TestValidators(TestCase):
+    def test_validate_url_returns_image(self) -> None:
+        # example.com should not be an image
+        with self.assertRaises(ValidationError):
+            validate_url_returns_image("https://example.com")
+
+        # invalid URLs should raise an error
+        with self.assertRaises(ValidationError):
+            validate_url_returns_image("invalid-url")
+        # imgur links should be images
+        validate_url_returns_image("https://i.imgur.com/k7XVwpB.jpeg")
--- src/core/utils/validators.py
+++ src/core/utils/validators.py
@@ -8,8 +8,11 @@ import requests
 def validate_url_returns_image(url: str, url_verbose_name: str = "URL") -> None:
     """Check if the URL returns an image.
     Use url_verbose_name to provide a more informative error message. (eg: "Profile Image URL")"""
+    # from https://www.whatismybrowser.com/guides/the-latest-user-agent/firefox
+    firefox_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0"
     try:
-        response = requests.head(url, timeout=5)
+        response = requests.head(url, timeout=5, headers={
+            "User-Agent": firefox_user_agent, "accept": "image/*"})
     except requests.RequestException as e:
         # Doesn't come up in practice because the form catches this
         raise ValidationError(f"{url_verbose_name} '{url}' is invalid.")
