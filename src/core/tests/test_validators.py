500edd3680257899c9e75e2a7bbe6685c1d99c0f
Alec <50636059+alec-zaiane@users.noreply.github.com>
2025-03-19 17:19:52 +0000

    test: fix the profile image modification test

--- src/core/tests/test_validators.py
+++ src/core/tests/test_validators.py
@@ -16,3 +16,7 @@ class TestValidators(TestCase):
             validate_url_returns_image("invalid-url")
         # imgur links should be images
         validate_url_returns_image("https://i.imgur.com/k7XVwpB.jpeg")
+
+        # picsum links should be images
+        validate_url_returns_image(
+            "https://fastly.picsum.photos/id/767/200/200.jpg?hmac=xNXS1_JsopizJIWXid2sWabvtf3urglv1JXpbh6Ahvc")
