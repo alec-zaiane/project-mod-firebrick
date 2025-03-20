17fe06ac5ba52a920aaed5217f9394fd37ba7b7f
Tanveer Multani <tamultan@ualberta.ca>
2025-03-20 19:29:41 +0000

    refactor(api): got rid of host_node definition

--- src/user_management/models.py
+++ src/user_management/models.py
@@ -303,7 +303,6 @@ class FollowRequest(ApiObject):
         Author, related_name="follow_requests_sent", on_delete=models.CASCADE)
     followee: models.ForeignKey[Author, Author] = models.ForeignKey(
         Author, related_name="follow_requests_received", on_delete=models.CASCADE)
-    host_node = models.ForeignKey('user_management.Node', on_delete=models.CASCADE)  
 
     objects: FollowRequestManager = FollowRequestManager()
 
