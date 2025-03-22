fcf13117f934dbc50f77c09cc8ee5705362c9711
Alec <50636059+alec-zaiane@users.noreply.github.com>
2025-03-22 21:49:39 +0000

    feat: commenting working at minimum level

--- src/comments/views.py
+++ src/comments/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
+
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+
+
+from comments.models import Comment
+from posts.models import Post, PostTypes
+from core.utils.request_viewer import get_request_viewer
+
+
 # Create your views here.
+
+
+class InternalCommentView(APIView):
+    def post(self, request: Request, encoded_post_fqid: str) -> Response:
+        viewer = get_request_viewer(request)
+        if viewer is None:
+            return Response({"detail": "not logged in as an author"}, status=401)
+        post = Post.objects.find_by_encoded_fqid(encoded_post_fqid)
+        if post is None or not post.check_can_be_seen_by(viewer):
+            return Response({"detail": "post not found"}, status=404)
+        comment_text = request.data.get("comment")
+        if comment_text is None:
+            return Response({"detail": "comment is required"}, status=400)
+        Comment.objects.create_comment(viewer, post, comment_text, PostTypes.PLAINTEXT)
+        return Response({"detail": "comment created"}, status=201)
