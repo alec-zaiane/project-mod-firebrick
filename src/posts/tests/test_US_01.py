a3b2da297899ef9a2962476aa0f633faf6e48efd
Alec <50636059+alec-zaiane@users.noreply.github.com>
2025-03-22 00:33:01 +0000

    test: add/unskip tests for US 01 and 21

--- src/posts/tests/test_US_01.py
+++ src/posts/tests/test_US_01.py
@@ -8,6 +8,8 @@ from core.utils.testing_utils import GeneralUserStoryApiTest, UITestCase
 
 from selenium.webdriver.common.alert import Alert
 
+from posts.models import Post
+
 
 @tag("US-posting")
 class TestUserStory01(GeneralUserStoryApiTest):
@@ -58,6 +60,41 @@ class TestUserStory01UI(UITestCase):
     """
     UI Tests for User Story 01
     """
+
+    def test_ui_can_get_to_create_post(self) -> None:
+        """Check that the user can get to the post creation via UI"""
+        self.initialize_sample_authors(1)
+        self.login_as(self.sample_authors[0])
+        self.visit(reverse("posts:stream"))
+        self.find_element_by_id("add-post-floating-button").click()
+        self.assert_path(reverse("posts:create_post"))
+        self.end_test()
+
+    def test_ui_can_get_to_create_post_from_author_profile(self) -> None:
+        """Check that the user can get to the post creation via their profile"""
+        self.initialize_sample_authors(1)
+        self.login_as(self.sample_authors[0])
+        self.visit(reverse("user_management:author_profile", args=[self.sample_authors[0].uuid]))
+        self.find_element_by_id("profile-create-post").find_element_by_selector("a").click()
+        self.assert_path(reverse("posts:create_post"))
+        self.end_test()
+
+    def test_ui_can_create_post(self) -> None:
+        self.initialize_sample_authors(1)
+        self.login_as(self.sample_authors[0])
+        self.visit(reverse("posts:create_post"))
+        self.find_element_by_id("id_title").send_keys("Test Post")
+        self.find_element_by_id("id_description").send_keys("Test Description")
+        self.find_element_by_id("id_content").send_keys("Test Content")
+        self.find_element_by_id("create-post-submit").click()
+        self.assertEqual(Post.visible_posts.count(), 1)
+        post = Post.visible_posts.first()
+        assert post is not None  # for mypy
+        self.assertEqual(post.title, "Test Post")
+        self.assertEqual(post.description, "Test Description")
+        self.assertEqual(post.content, "Test Content")
+        self.assertEqual(post.author, self.sample_authors[0])
+        self.end_test()
 
     def test_can_delete_post_ui(self) -> None:
         """Check that the user can delete a post through the UI"""
--- src/posts/tests/test_US_21.py
+++ src/posts/tests/test_US_21.py
@@ -5,24 +5,36 @@ from rest_framework import status
 
 from posts.models import Post, VisibilityTypes
 
-from core.utils.testing_utils import GeneralUserStoryApiTest
+from core.utils.testing_utils import UITestCase
 
 
-from unittest import skip
-
-
-@skip("Not implemented")
 @tag("US-Visibility")
-class TestUserStory21(GeneralUserStoryApiTest):
+class TestUserStory21(UITestCase):
     """
     Tests for User Story 21
     https://github.com/uofa-cmput404/w25-project-mod-firebrick/issues/21
     As an author, I want to be able to make my posts "friends-only," so that I don't have to worry about people I don't know seeing them.
     """
 
-    @tag("check-fast")
+    @tag("check-slow")
     def test_make_post_friends_only(self) -> None:
         """Test that an author can make a post friends-only"""
         self.initialize_sample_authors(1)
+        self.login_as(self.sample_authors[0])
+        self.visit(reverse("posts:create_post"))
+        self.find_element_by_id("id_title").send_keys("Test Post")
+        self.find_element_by_id("id_content").send_keys("Test Content")
 
-        # TODO
+        # use the dropdown to choose "friends only"
+        self.find_element_by_id("id_visibility_type").as_selector_choose_value("Friends Only")
+
+        # submit
+        self.find_element_by_id("create-post-submit").click()
+        self.assertEqual(Post.visible_posts.count(), 1)
+        post = Post.visible_posts.first()
+        assert post is not None  # for mypy
+        self.assertEqual(post.title, "Test Post")
+        self.assertEqual(post.content, "Test Content")
+        self.assertEqual(post.author, self.sample_authors[0])
+        self.assertEqual(post.visibility_type, VisibilityTypes.FRIENDS_ONLY)
+        self.end_test()
