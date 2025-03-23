1c98d2b6ab637f9bb1abc14663f9794062ec309a
ravynhardcastle <16183076+ravynhardcastle@users.noreply.github.com>
2025-03-23 02:41:44 -0600

    feat: default redirect to stream

--- src/core/urls.py
+++ src/core/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="stream")),
     path("admin/", admin.site.urls),
     path("", include("posts.urls")),
     path("", include("user_management.urls")),
