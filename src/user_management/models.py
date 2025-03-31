e78a22aedc79329ebd7577bb45e7b09ba1c2c1a1
Alec <50636059+alec-zaiane@users.noreply.github.com>
2025-03-31 17:58:28 +0000

    fix: stringify traceback

--- src/user_management/models.py
+++ src/user_management/models.py
@@ -579,7 +579,7 @@ class Node(models.Model):
                 AuthorSerializer().get_or_create(author)
             except Exception as e:
                 print(f"[Node {self.name}] Failed to synchronize author {author}: {e}")
-                print(f"\t {e.__class__}, {e.__traceback__}")
+                print(f"\t {e.__class__}, {str(e.__traceback__)}")
                 continue
 
     def _synchronize_posts(self) -> None:
