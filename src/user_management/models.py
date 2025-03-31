534c1044d918a8dc5a64963a3e51b371f1800df4
dottemeod <mirimaadkhan@gmail.com>
2025-03-31 07:19:47 +0000

    endpoint fix for sync likes

--- src/user_management/models.py
+++ src/user_management/models.py
@@ -617,7 +617,7 @@ class Node(models.Model):
         print(f"[Node {self.name}] Synchronizing likes for {len(hosted_users)} authors...")
         for author in hosted_users:
             response = requests.get(
-                self._make_absolute_url(f"/authors/{author.get_encoded_fqid()}/likes"),
+                self._make_absolute_url(f"/authors/{author.get_encoded_fqid()}/liked"),
                 headers={"Accept": "application/json"},
                 auth=HTTPBasicAuth(self.internal_user.username, self.internal_user.password_plain)
             )
