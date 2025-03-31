7b58685b0066617dcf9e202e32604bb30bd457ef
reubenjosephjohn <rjjohn1@ualberta.ca>
2025-03-31 11:16:40 +0000

    supporting likes sync with ext nodes

--- src/likes/urls.py
+++ src/likes/urls.py
@@ -1,5 +1,6 @@
 from rest_framework import routers
 from django.urls import URLPattern, URLResolver, path
+from likes.views import AuthorLikesAPIView
 
 from likes.viewsets import LikeViewSet
 from likes import views
@@ -7,6 +8,7 @@ from likes import views
 app_name = "likes"
 urlpatterns: list[URLPattern | URLResolver] = [
     path("api/likes/<str:target_fqid>/like", views.LikeByViewer.as_view(), name="like_by_viewer"),
+    path("authors/<str:encoded_author_fqid>/liked", AuthorLikesAPIView.as_view(), name="node2node_author_likes"),
 ]
 
 router = routers.SimpleRouter()
